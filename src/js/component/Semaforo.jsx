85355a2b9c7a1c13038cc111ee9498b01f33d10e
Raúl Santana Castilla <168531613+RaulSantanaCastilla@users.noreply.github.com>
2024-08-06 12:18:27 +0000

    Semaforo con boton cambiar de color

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -3,28 +3,41 @@ import React, { useState } from "react";
 const Semaforo = () => {
 
     const [encender, setEncender] = useState("Rojo");
-    const cambioColor = (color) =>{
+
+    const cambioColor = (color) => {
         setEncender(color);
-    }
+    };
+
+    const alternarColor = () => {
+        if (encender === "Rojo") {
+            setEncender("Amarillo");
+        } else if (encender === "Amarillo") {
+            setEncender("Verde");
+        } else if (encender === "Verde") {
+            setEncender("Rojo");
+        }
+    };
+
     return (
         <div className="Contenedor">
             <div className="base-semaforo"></div>
             <div className="base-luces">
                 <div 
-                className={`Rojo ${encender == "Rojo" ? "luz-encendida" : "" }`}
-                onClick={()=>cambioColor("Rojo")}
+                    className={`Rojo ${encender === "Rojo" ? "luz-encendida" : ""}`}
+                    onClick={() => cambioColor("Rojo")}
                 ></div>
                 <div 
-                className={`Amarillo ${encender == "Amarillo" ? "luz-encendida" : "" }`}
-                onClick={()=>cambioColor("Amarillo")}
+                    className={`Amarillo ${encender === "Amarillo" ? "luz-encendida" : ""}`}
+                    onClick={() => cambioColor("Amarillo")}
                 ></div>
                 <div 
-                className={`Verde ${encender == "Verde" ? "luz-encendida" : "" }`}
-                onClick={()=>cambioColor("Verde")}
+                    className={`Verde ${encender === "Verde" ? "luz-encendida" : ""}`}
+                    onClick={() => cambioColor("Verde")}
                 ></div>
             </div>
-        </div>  
-    )
-}
+            <button onClick={alternarColor}>Cambiar de color</button>
+        </div>
+    );
+};
 
 export default Semaforo;
